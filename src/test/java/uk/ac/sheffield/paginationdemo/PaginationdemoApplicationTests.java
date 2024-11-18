4bc16f434c357ea9abc2a18e67ed79d089d8c5d9
James Blackburn <blackburnfjames@gmail.com>
2024-11-18 11:59:10 +0000

    chore: project layout

---
+++ src/test/java/uk/ac/sheffield/paginationdemo/PaginationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.sheffield.paginationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
